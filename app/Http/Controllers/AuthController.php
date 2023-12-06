d082934bb295b7aaed130822edaaa851cc279da7
Vitor Guarilha <vitorguarilha@poli.ufrj.br>
2023-12-06 15:08:51 -0300

    feat: feat: Adicionar documentação da API's através do Swagger.
    Neste commit, foi realizada a implementação do Swagger para documentar os endpoints. Isso inclui a instalação do pacote Swaggr e toda a estrutura de documentação.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,7 +5,28 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
-{
+
+/**
+ * @OA\Info(
+ *   title="API Laravel Swagger Documentation - Projeto Jump Park",
+ *   version="1.5.0",
+ *   contact={
+ *     "email": "vitorguarilha@poli.ufrj.br"
+ *   }
+ * )
+ * @OA\SecurityScheme(
+ *  type="http",
+ *  description="Acess token obtido na autenticação",
+ *  name="Authorization",
+ *  in="header",
+ *  scheme="bearer",
+ *  bearerFormat="JWT",
+ *  securityScheme="bearerToken"
+ * )
+ */
+
+{   
+
     /**
      * Get a JWT via given credentials.
      *
@@ -28,6 +49,40 @@ class AuthController extends Controller
      * @param  string $token
      *
      * @return \Illuminate\Http\JsonResponse
+     */
+
+     /**
+     * @OA\POST(
+     *  tags={"LOGIN - JWT Token Authentication"},
+     *  summary="Obter um token de autenticação de usuário",
+     *  description="Esse endpoint retorna um novo token para ser usado nas rotas protegidas",
+     *  path="/api/login",
+     *  @OA\RequestBody(
+     *      @OA\MediaType(
+     *          mediaType="application/json",
+     *          @OA\Schema(
+     *              required={"email","password"},
+     *              @OA\Property(property="email", type="string", example="vitorguarilha@poli.ufrj.br"),
+     *              @OA\Property(property="password", type="string", example="12345678"),
+     *          )
+     *      ),
+     *  ),
+     *  @OA\Response(
+     *    response=200,
+     *    description="Token gerado com sucesso",
+     *    @OA\JsonContent(
+     *       @OA\Property(property="plainTextToken", type="string", example="2|MZEBxLy1zulPtND6brlf8GOPy57Q4DwYunlibXGj")
+     *    )
+     *  ),
+     *  @OA\Response(
+     *    response=422,
+     *    description="Usuário e/ou senha incorreta",
+     *    @OA\JsonContent(
+     *       @OA\Property(property="message", type="string", example="The provided credentials are incorrect."),
+     *       @OA\Property(property="errors", type="string", example="..."),
+     *    )
+     *  )
+     * )
      */
     protected function respondWithToken($token)
     {
--- app/Http/Controllers/ServiceOrderController.php
+++ app/Http/Controllers/ServiceOrderController.php
@@ -22,20 +22,145 @@ class ServiceOrderController extends Controller
     {
         return $this->serviceOrderRepository->listarServiceOrder($request);
     }
+    
+    /**
+    * @OA\GET(
+    *  tags={"LISTAR"},
+    *  summary="Listar ordens de serviço",
+    *  description="Esse endpoint retorna uma lista das ordens de serviço cadastradas",
+    *  path="/api/service_order",
+    *  security={ {"bearerToken":{}} },
+    *  @OA\Parameter(
+    *      name="page",
+    *      in="query",
+    *      description="Número da página",
+    *      required=false,
+    *      @OA\Schema(type="integer")
+    *  ),
+    *  @OA\Parameter(
+    *      name="vehiclePlate",
+    *      in="query",
+    *      description="Placa do veículo",
+    *      required=false,
+    *      @OA\Schema(type="string")
+    *  ),
+    *  @OA\Response(
+    *    response=200,
+    *    description="Operação concluída com sucesso",
+    *  )
+    * )
+    */
 
     public function criarServiceOrder(ServiceOrderRequest $request)
     {
         return $this->serviceOrderRepository->criarServiceOrder($request);
     }
 
+    /**
+    * @OA\POST(
+    *   tags={"CRIAR"},
+    *   summary="Criar nova ordem de serviço",
+    *   description="Esse endpoint cria uma nova ordem de serviço",
+    *   path="/api/service_order",
+    *   security={ {"bearerToken":{}} },
+    *   @OA\RequestBody(
+    *      @OA\MediaType(
+    *          mediaType="application/json",
+    *          @OA\Schema(
+    *              required={"vehiclePlate", "entryDateTime", "exitDateTime", "priceType", "price", "userId"},
+    *              @OA\Property(property="vehiclePlate", type="char", example="VVV7777"),
+    *              @OA\Property(property="entryDateTime", type="dateTime", example="0001-01-01 00:00:00"),
+    *              @OA\Property(property="exitDateTime", type="string", example="0001-01-01 00:00:00"),
+    *              @OA\Property(property="priceType", type="string", example="NOITE"),
+    *              @OA\Property(property="price", type="decimal", example="40"),
+    *              @OA\Property(property="userId", type="unsignedBigInteger", example="1"),
+    *          )
+    *      ),
+    *   ),
+    *   @OA\Response(
+    *      response=200,
+    *      description="Ordem de serviço criada com sucesso",
+    *   ),
+    *   @OA\Response(
+    *      response=422,
+    *      description="Erro de validação",
+    *   )
+    * )
+    */
+
     public function editarServiceOrder(ServiceOrderRequest $request, $serviceOrderId)
     {
         return $this->serviceOrderRepository->editarServiceOrder($request, $serviceOrderId);
     }
 
+    /**
+    * @OA\PUT(
+    *   tags={"EDITAR"},
+    *   summary="Editar ordem de serviço",
+    *   description="Esse endpoint edita uma ordem de serviço",
+    *   path="/api/service_order/{service_order_id}",
+    *   security={ {"bearerToken":{}} },
+    *   @OA\Parameter(
+    *      name="service_order_id",
+    *      in="path",
+    *      description="ID da ordem de serviço a ser editada",
+    *      required=true,
+    *      @OA\Schema(
+    *          type="integer",
+    *          format="int64"
+    *      ),
+    *   ),
+    *   @OA\RequestBody(
+    *      @OA\MediaType(
+    *          mediaType="application/json",
+    *          @OA\Schema(
+    *              @OA\Property(property="vehiclePlate", type="char", example="VVV7777"),
+    *              @OA\Property(property="price", type="decimal", example="40"),
+    *          )
+    *      ),
+    *   ),
+    *   @OA\Response(
+    *      response=200,
+    *      description="Ordem de serviço editada com sucesso",
+    *   ),
+    *   @OA\Response(
+    *      response=422,
+    *      description="Erro de validação",
+    *   )
+    * )
+    */
+
     public function deletarServiceOrder($serviceOrderId)
     { 
         return $this->serviceOrderRepository->deletarServiceOrder($serviceOrderId);
     }
+
+    /**
+    * @OA\DELETE(
+    *   tags={"DELETAR"},
+    *   summary="Deletar ordem de serviço",
+    *   description="Esse endpoint deleta uma ordem de serviço",
+    *   path="/api/service_order/{serviceOrderId}",
+    *   security={ {"bearerToken":{}} },
+    *   @OA\Parameter(
+    *      name="serviceOrderId",
+    *      in="path",
+    *      description="ID da ordem de serviço a ser deletada",
+    *      required=true,
+    *      @OA\Schema(
+    *          type="integer",
+    *          format="int64"
+    *      ),
+    *   ),
+    *   @OA\Response(
+    *      response=200,
+    *      description="Ordem de serviço deletada com sucesso",
+    *   ),
+    *   @OA\Response(
+    *      response=422,
+    *      description="Erro de validação",
+    *   )
+    * )
+    */
 
 }
